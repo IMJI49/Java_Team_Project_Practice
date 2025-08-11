73cc600453d2001135f78e2643f4dbd9b0c0002c
Gilbert013 <50778722+Gilbert013@users.noreply.github.com>
2025-08-11 23:44:54 +0900

    update ui

--- src/com/shoppingmall/ShoppingMallSystem.java
+++ src/com/shoppingmall/ShoppingMallSystem.java
@@ -1,9 +1,12 @@
 package com.shoppingmall;
 
+import com.shoppingmall.controller.MainController;
+
 public class ShoppingMallSystem {
 
 	public static void main(String[] args) {
-
+		MainController mainController = new MainController();
+		mainController.start();
 	}
 
 }
